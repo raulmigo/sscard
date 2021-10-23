09623938d7ee1da3fc5cfde81290d90bd6569ee1
rmg1692 <rmg1692@hotmail.com>
2021-10-22 21:02:20 -0400

    create card.js

---
+++ card.js
@@ -0,0 +1,58 @@
+let name = document.getElementById('name').textContent
+
+let barcode = document.getElementById('clubid').textContent;
+	
+	JsBarcode("#barcode", barcode, {
+  format: "CODE128",
+  lineColor: "#000000",
+  width:4,
+  height:80,
+  displayValue: true
+});
+
+Webcam.attach( '#photo' );
+
+function take_snapshot() {
+	console.log("snapshot clicked!!")
+			// take snapshot and get image data
+	Webcam.snap(function(data_uri) {
+				// display results in page
+				document.getElementById('photo').innerHTML ='<img src="'+data_uri+'"/>';
+			});
+}
+
+//Function To Display Popup
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
+
+
+console.log(name);
+	//document.getElementById('clubid')
+	//document.getElementById('expiration')
+function fillOutCard() {
+	div_hide();
+	document.getElementById('name').textContent = document.getElementById('formname').value;
+	
+	document.getElementById('clubid').textContent = "Club ID# " + document.getElementById('memberId').value;
+	
+	document.getElementById('issueDate').textContent = "Issue Date: " + document.getElementById('issue').value;
+	
+	let barcode = document.getElementById('memberId').value;
+	
+	JsBarcode("#barcode", barcode, {
+  format: "CODE128",
+  lineColor: "#000000",
+  width:4,
+  height:80,
+  displayValue: true
+});
+	
+	
+	//console.log(document.getElementById("formname"));	
+}
+
